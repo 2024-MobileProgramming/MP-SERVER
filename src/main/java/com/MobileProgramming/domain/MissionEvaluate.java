4d29ebd284e8e5aed697c383888cb026384b5210
Choi Min Young <codingmy@naver.com>
2024-04-07 17:28:34 +0900

    [FEAT/#7] MissionEvaluate 메소드 구현

--- src/main/java/com/MobileProgramming/domain/MissionEvaluate.java
+++ src/main/java/com/MobileProgramming/domain/MissionEvaluate.java
@@ -2,11 +2,10 @@ package com.MobileProgramming.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 
 @Entity
 @Slf4j
@@ -23,13 +22,26 @@ public class MissionEvaluate {
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User submitUser;
 
-    @OneToMany(mappedBy = "user_id")
-    @Column(name = "host_id")
-    private List<User> evaluator;
+    @ManyToOne
+    @JoinColumn(name = "host_id", referencedColumnName = "user_id")
+    private User evaluator;
 
     @Column(name = "mission_id")
     private Integer missionId;
 
     @Column(name = "checked")
-    private Boolean checked;
+    private Boolean approval;
+
+    @Builder
+    public MissionEvaluate(User user, User evaluator, Integer missionId) {
+        this.submitUser = user;
+        this.evaluator = evaluator;
+        this.missionId = missionId;
+        this.approval = false;
+    }
+
+    //평가유저가 미션 성공으로 판단시
+    public void updateApproval() {
+        approval = true;
+    }
 }
