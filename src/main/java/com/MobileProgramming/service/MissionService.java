c5db11ccae5b08864ffcb1d59c4040e96488afad
Choi Min Young <codingmy@naver.com>
2024-05-16 01:56:33 +0900

    [FEAT/#18] postMissionVerficate 서비스 구현

--- src/main/java/com/MobileProgramming/service/MissionService.java
+++ src/main/java/com/MobileProgramming/service/MissionService.java
@@ -1,5 +1,6 @@
 package com.MobileProgramming.service;
 
+import com.MobileProgramming.dto.request.PostMissionVerficateRequest;
 import com.MobileProgramming.dto.response.GetMissionDataResponse;
 import com.MobileProgramming.repository.JPA.MissionRepositoryImpl;
 import jakarta.transaction.Transactional;
@@ -10,17 +11,22 @@ import static com.MobileProgramming.exception.ErrorMessage.GOAL_NOT_FOUND_EXCEPT
 @Service
 @Transactional
 public class MissionService {
-    private final MissionRepositoryImpl jpaMissionRepository;
+    private final MissionRepositoryImpl jpaMissionRepositoryImpl;
 
-    public MissionService(MissionRepositoryImpl jpaMissionRepository) {
-        this.jpaMissionRepository = jpaMissionRepository;
+    public MissionService(MissionRepositoryImpl jpaMissionRepositoryImpl) {
+        this.jpaMissionRepositoryImpl = jpaMissionRepositoryImpl;
     }
 
     //특정유저의 1개의 미션에 대한 정보 get method
     @Transactional
     public GetMissionDataResponse getMissionData(int userId, int missionId) throws Exception {
-        if (jpaMissionRepository.getMissionData(userId, missionId) == null)
+        if (jpaMissionRepositoryImpl.getMissionData(userId, missionId) == null)
             throw new Exception(GOAL_NOT_FOUND_EXCEPTION.getMessage());
-            return jpaMissionRepository.getMissionData(userId, missionId);
+        return jpaMissionRepositoryImpl.getMissionData(userId, missionId);
+    }
+
+    @Transactional
+    public boolean postMissionVerificate(PostMissionVerficateRequest request) {
+        return jpaMissionRepositoryImpl.postMissionVerificate(request.verificaterUserId(), request.verificatedUserId(), request.missionId());
     }
 }
