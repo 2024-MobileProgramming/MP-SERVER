721a6bb78d096ec31bf52bd3c533e06549818cba
Choi Min Young <codingmy@naver.com>
2024-05-30 13:16:20 +0900

    [FEAT/#38] User 테이블 수정

--- src/main/java/com/MobileProgramming/domain/User.java
+++ src/main/java/com/MobileProgramming/domain/User.java
@@ -1,10 +1,7 @@
 package com.MobileProgramming.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
@@ -13,16 +10,12 @@ import java.util.Objects;
 @Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@Table(name = "\"User\"", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_name", "user_nickname", "user_kakao_email", "user_phone"}, name = "unique_user_info")})
-//DB가 수정됨에 따라 수정되는데 제가 위에 꺼 잘 몰라서 일단 주석처리했습니다. 필요한거라면 다시 주석 풀고 맞게 바꾸면 될 것 같아요.
+@EqualsAndHashCode
 public class User {
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
-
-    @Column(name = "name", nullable = false)
-    private String name;
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
@@ -30,38 +23,10 @@ public class User {
     @Column(name = "email", unique = true)
     private String email;
 
-    @Column(name = "phone_number", unique = true)
-    private String phoneNumber;
-
     @Builder
-    public User(String name, String nickname, String email, String phoneNumber) {
-        this.name = name;
+    public User(String nickname, String email) {
         this.email = email;
-        this.phoneNumber = phoneNumber;
         this.nickname = nickname;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(userId, user.userId) && Objects.equals(name, user.name) && Objects.equals(nickname, user.nickname) && Objects.equals(email, user.email) && Objects.equals(phoneNumber, user.phoneNumber);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(userId, name, nickname, email, phoneNumber);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "userId=" + userId +
-                ", name='" + name + '\'' +
-                ", nickname='" + nickname + '\'' +
-                ", email='" + email + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                '}';
-    }
 }
