1269c4fb6662c14e378db2d469d915569d82831d
Choi Min Young <codingmy@naver.com>
2024-06-03 17:31:51 +0900

    [FEAT/#38] 가입 유저 db 저장 로직

--- src/main/java/com/MobileProgramming/controller/LoginController.java
+++ src/main/java/com/MobileProgramming/controller/LoginController.java
@@ -1,15 +1,40 @@
 package com.MobileProgramming.controller;
 
 
+import com.MobileProgramming.domain.User;
+import com.MobileProgramming.dto.request.LoginRequestDto;
+import com.MobileProgramming.dto.response.LoginResponseDto;
+import com.MobileProgramming.exception.SuccessMessage;
+import com.MobileProgramming.global.response.ApiResponse;
+import com.MobileProgramming.repository.JPA.JPAUserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
 @AllArgsConstructor
+@RequestMapping("/login")
 public class LoginController {
+    private JPAUserRepository jpaUserRepository;
 
+    @PostMapping
+    public ApiResponse<LoginResponseDto> login(@RequestBody LoginRequestDto UserInfo) {
+        String email = UserInfo.getEmail();
+        String nickname = UserInfo.getNickname();
+        List<Integer> userIdByEmail = jpaUserRepository.findUserIdByEmail(email);
+        if (userIdByEmail.isEmpty()) {
+            jpaUserRepository.saveUser(new User(nickname, email));
+
+        }
+        return ApiResponse.success(SuccessMessage.USER_CREATE_SUCCESS, new LoginResponseDto(jpaUserRepository.findUserIdByEmail(email).get(0)));
+
+    }
 
 
 }
